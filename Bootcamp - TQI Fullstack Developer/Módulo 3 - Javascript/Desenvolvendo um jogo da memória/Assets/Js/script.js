08bc4800d672be00ec8fa1ff9e3c58d60ff903d7
Leonnardo <leonnardobtrindade@hotmail.com>
2022-05-19 09:43:57 -0300

    Adiciona arquivos referentes ao jogo - Quebra Cabeça

--- Bootcamp - TQI Fullstack Developer/Módulo 3 - Javascript/Desenvolvendo um jogo da memória/Assets/Js/script.js
+++ Bootcamp - TQI Fullstack Developer/Módulo 3 - Javascript/Desenvolvendo um jogo da memória/Assets/Js/script.js
@@ -5,6 +5,9 @@ let firstCard, secondCard;
 let lockBoard = false;
 
 function flipCard(){
+    if(lockBoard) return;
+    if (this === firstCard) return;
+
     this.classList.add('flip');
 
     if(!hasFlipedCard){
@@ -30,14 +33,32 @@ function checkForMath(){
 function disableCards(){
     firstCard.removeEventListener('click', flipCard)
     secondCard.removeEventListener('click', flipCard)
+
+    resetBoard();
 }
 
 function unflipCard(){
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        resetBoard();
     }, 1500);
 }
+
+function resetBoard(){
+    [hasFlipedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle(){
+    cards.forEach((card) => {
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition;
+    })
+})();
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard);
