5b1db384db8abbc337d87c67da52557a7b575ef6
Leonnardo <leonnardobtrindade@hotmail.com>
2022-05-25 14:12:49 -0300

    Adiciona arquivos referentes as aulas

---
+++ Bootcamp - TQI Fullstack Developer/Módulo 5 - SQL/MySql - Trabalhando com as suas primeiras tabelas/Inserindo dados na tabela/dio.sql
@@ -0,0 +1,6 @@
+CREATE TABLE pessoas(id INT NOT NULL PRIMARY KEY AUTOINCREMENT, nome VARCHAR(30) NOT NULL, 
+nascimento DATE)
+
+INSERT INTO pessoas (nome, nascimento) VALUES ('Leonnardo', '2003-12-20')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Paulo', '1997-11-17')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Sergio', '2005-08-25')
